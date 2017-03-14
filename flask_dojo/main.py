f7062ce23cde816b92abbec4c87f5df1722c17dc
Dubson69 <dubielgr@gmail.com>
2017-03-14 10:01:38 +0100

    get_counter works

--- flask_dojo/main.py
+++ flask_dojo/main.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+import data_manager
 
 app = Flask(__name__)
+
+@app.route("/request-counter", methods=['GET', 'POST'])
+def increment():
+    if request.method == "GET":
+        get_counter = data_manager.show_get_counter()[0]
+        return get_counter
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
